8a55d7dab6be0611ab341bbff40c05c922c684bf
Dmitry Rotay <dmitry.rotay@gmail.com>
2020-08-20 18:47:12 +1000

    Add 4-matrix multiplication

--- src/Matrix.hs
+++ src/Matrix.hs
@@ -2,6 +2,8 @@ module Matrix
     ( SquareMatrix2 (..)
     , SquareMatrix3 (..)
     , SquareMatrix4 (..)
+    , multiply4
+    , multiplyTuple4
     ) where
 
 import Common
@@ -53,3 +55,52 @@ instance Eq SquareMatrix4 where
            && m01 ~== n01 && m11 ~== n11 && m21 ~== n21 && m31 ~== n31
            && m02 ~== n02 && m12 ~== n12 && m22 ~== n22 && m32 ~== n32
            && m03 ~== n03 && m13 ~== n13 && m23 ~== n23 && m33 ~== n33
+
+newtype MatrixArray = MatrixArray [[Float]]
+
+toRowsArray4 :: SquareMatrix4 -> MatrixArray
+toRowsArray4 (SquareMatrix4 ((m00, m10, m20, m30),
+                             (m01, m11, m21, m31),
+                             (m02, m12, m22, m32),
+                             (m03, m13, m23, m33))) =
+                               MatrixArray
+                               [[m00, m10, m20, m30],
+                                [m01, m11, m21, m31],
+                                [m02, m12, m22, m32],
+                                [m03, m13, m23, m33]]
+
+toColsArray4 :: SquareMatrix4 -> MatrixArray
+toColsArray4 (SquareMatrix4 ((m00, m10, m20, m30),
+                             (m01, m11, m21, m31),
+                             (m02, m12, m22, m32),
+                             (m03, m13, m23, m33))) =
+                               MatrixArray
+                                [[m00, m01, m02, m03],
+                                [m10, m11, m12, m13],
+                                [m20, m21, m22, m23],
+                                [m30, m31, m32, m33]]
+
+cross :: [Float] -> [Float] -> Float
+cross a1 a2 = sum (zipWith (*) a1 a2)
+
+multiply4 :: SquareMatrix4 -> SquareMatrix4 -> SquareMatrix4
+m1 `multiply4` m2 = 
+    let MatrixArray rows1 = toRowsArray4 m1
+        MatrixArray cols2 = toColsArray4 m2
+        productMatrixArray = [[cross row1 col2 | col2 <- cols2] | row1 <- rows1]
+        [[m00, m10, m20, m30],
+         [m01, m11, m21, m31],
+         [m02, m12, m22, m32],
+         [m03, m13, m23, m33]] = productMatrixArray
+    in SquareMatrix4 ((m00, m10, m20, m30),
+                      (m01, m11, m21, m31),
+                      (m02, m12, m22, m32),
+                      (m03, m13, m23, m33))
+
+type Tuple4 = (Float, Float, Float, Float)
+
+multiplyTuple4 :: SquareMatrix4 -> Tuple4 -> Tuple4
+multiplyTuple4 m (t0, t1, t2, t3) =
+    let MatrixArray [row0, row1, row2, row3] = toRowsArray4 m
+        a = [t0, t1, t2, t3]
+    in (cross row0 a, cross row1 a, cross row2 a, cross row3 a)
--- test/MatrixSpec.hs
+++ test/MatrixSpec.hs
@@ -34,3 +34,25 @@ spec = do
                                         (104.04, 27.04, 104.04, 27.04),
                                         (104.04, 27.04, 104.04, 27.04))
                 in m1 `shouldBe` m2
+            it "multiplication of two matrices produces valid product matrix" $
+                let m1 = SquareMatrix4 ((1, 2, 3, 4),
+                                        (5, 6, 7, 8),
+                                        (9, 8, 7, 6),
+                                        (5, 4, 3, 2))
+                    m2 = SquareMatrix4 ((-2, 1, 2, 3),
+                                        (3, 2, 1, -1),
+                                        (4, 3, 6, 5),
+                                        (1, 2, 7, 8))
+                    expected = SquareMatrix4 ((20, 22, 50, 48),
+                                              (44, 54, 114, 108),
+                                              (40, 58, 110, 102),
+                                              (16, 26, 46, 42))
+                in m1 `multiply4` m2 `shouldBe` expected
+            it "multiplication of matrix by tuple produces valid product tuple" $
+                let m = SquareMatrix4 ((1, 2, 3, 4),
+                                       (2, 4, 4, 2),
+                                       (8, 6, 4, 1),
+                                       (0, 0, 0, 1))
+                    t = (1, 2, 3, 1)
+                    expected = (18, 24, 33, 1)
+                in m `multiplyTuple4` t `shouldBe` expected
