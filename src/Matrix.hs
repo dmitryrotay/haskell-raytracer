d8eb86cfd3450b8aef65ccca962c271cd087e676
Dmitry Rotay <dmitry.rotay@gmail.com>
2020-08-22 10:06:45 +1000

    Implement transpose, 2-determinant and submatrices

--- src/Matrix.hs
+++ src/Matrix.hs
@@ -1,9 +1,14 @@
 module Matrix
     ( SquareMatrix2 (..)
+    , determinant2
     , SquareMatrix3 (..)
+    , subMatrix3
     , SquareMatrix4 (..)
+    , subMatrix4
     , multiply4
     , multiplyTuple4
+    , identityMatrix4
+    , transpose4
     ) where
 
 import Common
@@ -20,6 +25,11 @@ instance Eq SquareMatrix2 where
         in    m00 ~== n00 && m10 ~== n10
            && m01 ~== n01 && m11 ~== n11
 
+determinant2 :: SquareMatrix2 -> Float
+determinant2 (SquareMatrix2 ((m00, m10),
+                            (m01, m11))) =
+                                m00 * m11 - m10 * m01
+
 newtype SquareMatrix3 = SquareMatrix3 ((Float, Float, Float),
                                        (Float, Float, Float),
                                        (Float, Float, Float)) deriving (Show)
@@ -35,6 +45,21 @@ instance Eq SquareMatrix3 where
         in    m00 ~== n00 && m10 ~== n10 && m20 ~== n20
            && m01 ~== n01 && m11 ~== n11 && m21 ~== n21
            && m02 ~== n02 && m12 ~== n12 && m22 ~== n22
+
+toRowsArray3 :: SquareMatrix3 -> MatrixArray
+toRowsArray3 (SquareMatrix3 ((m00, m10, m20),
+                             (m01, m11, m21),
+                             (m02, m12, m22))) =
+                               MatrixArray [[m00, m10, m20],
+                                            [m01, m11, m21],
+                                            [m02, m12, m22]]
+
+subMatrix3 :: SquareMatrix3 -> Int -> Int -> SquareMatrix2
+subMatrix3 m removeRow removeCol =
+    let MatrixArray [[m00, m10],
+                     [m01, m11]] = matrixSubarray (toRowsArray3 m) removeRow removeCol
+    in SquareMatrix2 ((m00, m10),
+                      (m01, m11))
 
 newtype SquareMatrix4 = SquareMatrix4 ((Float, Float, Float, Float),
                                        (Float, Float, Float, Float),
@@ -56,29 +81,41 @@ instance Eq SquareMatrix4 where
            && m02 ~== n02 && m12 ~== n12 && m22 ~== n22 && m32 ~== n32
            && m03 ~== n03 && m13 ~== n13 && m23 ~== n23 && m33 ~== n33
 
+subMatrix4 :: SquareMatrix4 -> Int -> Int -> SquareMatrix3
+subMatrix4 m removeRow removeCol =
+    let MatrixArray [[m00, m10, m20],
+                     [m01, m11, m21],
+                     [m02, m12, m22]] = matrixSubarray (toRowsArray4 m) removeRow removeCol
+    in SquareMatrix3 ((m00, m10, m20),
+                      (m01, m11, m21),
+                      (m02, m12, m22)) 
+
 newtype MatrixArray = MatrixArray [[Float]]
+
+matrixSubarray :: MatrixArray -> Int -> Int -> MatrixArray
+matrixSubarray (MatrixArray m) removeRow removeCol = 
+    let rows = zip [0..] [zip [0..] row | row <- m]
+    in MatrixArray [[col | (colIndex, col) <- row, colIndex /= removeCol] | (rowIndex, row) <- rows, rowIndex /= removeRow]
 
 toRowsArray4 :: SquareMatrix4 -> MatrixArray
 toRowsArray4 (SquareMatrix4 ((m00, m10, m20, m30),
                              (m01, m11, m21, m31),
                              (m02, m12, m22, m32),
                              (m03, m13, m23, m33))) =
-                               MatrixArray
-                               [[m00, m10, m20, m30],
-                                [m01, m11, m21, m31],
-                                [m02, m12, m22, m32],
-                                [m03, m13, m23, m33]]
+                               MatrixArray [[m00, m10, m20, m30],
+                                            [m01, m11, m21, m31],
+                                            [m02, m12, m22, m32],
+                                            [m03, m13, m23, m33]]
 
 toColsArray4 :: SquareMatrix4 -> MatrixArray
 toColsArray4 (SquareMatrix4 ((m00, m10, m20, m30),
                              (m01, m11, m21, m31),
                              (m02, m12, m22, m32),
                              (m03, m13, m23, m33))) =
-                               MatrixArray
-                                [[m00, m01, m02, m03],
-                                [m10, m11, m12, m13],
-                                [m20, m21, m22, m23],
-                                [m30, m31, m32, m33]]
+                               MatrixArray [[m00, m01, m02, m03],
+                                            [m10, m11, m12, m13],
+                                            [m20, m21, m22, m23],
+                                            [m30, m31, m32, m33]]
 
 cross :: [Float] -> [Float] -> Float
 cross a1 a2 = sum (zipWith (*) a1 a2)
@@ -104,3 +141,19 @@ multiplyTuple4 m (t0, t1, t2, t3) =
     let MatrixArray [row0, row1, row2, row3] = toRowsArray4 m
         a = [t0, t1, t2, t3]
     in (cross row0 a, cross row1 a, cross row2 a, cross row3 a)
+
+identityMatrix4 :: SquareMatrix4
+identityMatrix4 = SquareMatrix4 ((1, 0, 0, 0),
+                                 (0, 1, 0, 0),
+                                 (0, 0, 1, 0),
+                                 (0, 0, 0, 1))
+
+transpose4 :: SquareMatrix4 -> SquareMatrix4
+transpose4 (SquareMatrix4 ((m00, m10, m20, m30),
+                           (m01, m11, m21, m31),
+                           (m02, m12, m22, m32),
+                           (m03, m13, m23, m33))) =
+                               SquareMatrix4 ((m00, m01, m02, m03),
+                                              (m10, m11, m12, m13),
+                                              (m20, m21, m22, m23),
+                                              (m30, m31, m32, m33))
--- test/MatrixSpec.hs
+++ test/MatrixSpec.hs
@@ -14,6 +14,10 @@ spec = do
                     m2 = SquareMatrix2 ((104.04, 27.04),
                                         (104.04, 27.04))
                 in m1 `shouldBe` m2
+            it "calculates determinant correctly" $
+                let m = SquareMatrix2 ((1, 5),
+                                       (-3, 2))
+                in determinant2 m == 17
         describe "SquareMatrix3" $ do
             it "uses float equality comparison" $
                 let m1 = SquareMatrix3 ((10.2^2, 5.2^2, 10.2^2),
@@ -23,6 +27,13 @@ spec = do
                                         (104.04, 27.04, 104.04),
                                         (104.04, 27.04, 104.04))
                 in m1 `shouldBe` m2
+            it "extracting submatrix returns 2x2 matrix with given row and column removed" $
+                let m = SquareMatrix3 ((1, 5, 0),
+                                       (-3, 2, 7),
+                                       (0, 6, -3))
+                    expected = SquareMatrix2 ((-3, 2),
+                                              (0, 6))
+                in subMatrix3 m 0 2 `shouldBe` expected
         describe "SquareMatrix4" $ do
             it "uses float equality comparison" $
                 let m1 = SquareMatrix4 ((10.2^2, 5.2^2, 10.2^2, 5.2^2),
@@ -48,7 +59,7 @@ spec = do
                                               (40, 58, 110, 102),
                                               (16, 26, 46, 42))
                 in m1 `multiply4` m2 `shouldBe` expected
-            it "multiplication of matrix by tuple produces valid product tuple" $
+            it "multiplication by tuple produces valid product tuple" $
                 let m = SquareMatrix4 ((1, 2, 3, 4),
                                        (2, 4, 4, 2),
                                        (8, 6, 4, 1),
@@ -56,3 +67,31 @@ spec = do
                     t = (1, 2, 3, 1)
                     expected = (18, 24, 33, 1)
                 in m `multiplyTuple4` t `shouldBe` expected
+            it "multiplication by identity matrix must return self" $
+                let m = SquareMatrix4 ((0, 1, 2, 4),
+                                       (1, 2, 4, 8),
+                                       (2, 4, 8, 16),
+                                       (4, 8, 16, 32))
+                in m `multiply4` identityMatrix4 `shouldBe` m
+            it "transpose produces matrix with swapped rows and columns" $
+                let m = SquareMatrix4 ((0, 9, 3, 0),
+                                       (9, 8, 0, 8),
+                                       (1, 8, 5, 3),
+                                       (0, 0, 5, 8))
+                    expected = SquareMatrix4 ((0, 9, 1, 0),
+                                              (9, 8, 8, 0),
+                                              (3, 0, 5, 5),
+                                              (0, 8, 3, 8))
+                in transpose4 m `shouldBe` expected
+            it "extracting submatrix returns 3x3 matrix with given row and column removed" $
+                let m = SquareMatrix4 ((-6, 1, 1, 6),
+                                       (-8, 5, 8, 6),
+                                       (-1, 0, 8, 2),
+                                       (-7, 1, -1, 1))
+                    expected = SquareMatrix3 ((-6, 1, 6),
+                                              (-8, 8, 6),
+                                              (-7, -1, 1))
+                in subMatrix4 m 2 1 `shouldBe` expected
+            it "transpose of identity matrix produces identity matrix" $
+                transpose4 identityMatrix4 `shouldBe` identityMatrix4
+            
