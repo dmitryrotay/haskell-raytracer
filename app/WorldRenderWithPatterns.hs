c8389fee31c71c056045f2b78292ca2b84c610c8
Dmitry Rotay <dmitry.rotay@gmail.com>
2020-09-28 08:32:42 +1000

    Add sample render with patterns

--- app/WorldRenderWithPatterns.hs
+++ app/WorldRenderWithPatterns.hs
@@ -9,7 +9,7 @@ import Lights (PointLight (..))
 import Objects.Materials (Material (..), defaultMaterial)
 import Objects.Patterns
     ( Fill (..)
-    , createCombinedCheckerPattern
+    , createBlendedPattern
     , createGradientPattern
     , createRingPattern
     , createStripePattern
@@ -22,8 +22,10 @@ import World (World (..))
 
 renderWorld :: IO ()
 renderWorld = do
-    let floorSubPattern = setPatternTransform (createGradientPattern (Color 1 1 1) (Color 0.3 0.3 0.3)) (rotationY (-pi / 2) |<>| scaling 30 30 30)
-        floorPattern = createCombinedCheckerPattern (PatternFill floorSubPattern) (SolidFill $ Color 0.24 0.24 0.55)
+    let stripePattern1 = createStripePattern (Color 0.3 0.3 0.3) (Color 0.5 0.5 1.0)
+        stripePattern2 = setPatternTransform stripePattern1 (rotationY (2 * pi / 3))
+        stripePattern3 = setPatternTransform stripePattern1 (rotationY (4 * pi / 3))
+        floorPattern = createBlendedPattern [PatternFill stripePattern1, PatternFill stripePattern2, PatternFill stripePattern3]
         floorMaterial = defaultMaterial { getColor = Color 1 0.9 0.9, getSpecular = 0, getPattern = Just floorPattern }
         (fl, fId) = createPlane 0
         floor' = setMaterial fl floorMaterial
@@ -41,7 +43,7 @@ renderWorld = do
                                 
         (rightSphere, rId) = createSphere mId
         rightSphereTransform = scaling 0.7 0.7 0.7 |<>| translation 1.5 0.5 (-0.5)
-        rightSpherePattern = setPatternTransform (createStripePattern (Color 0.5 1 0.1) (Color 1 1 1)) (rotationZ (pi / 2) |<>| rotationX (pi / 4))
+        rightSpherePattern = setPatternTransform (createStripePattern (Color 0.5 1 0.1) (Color 1 1 1)) (rotationZ (pi / 2) |<>| rotationX (pi / 6))
         rightSphereMaterial = defaultMaterial
                                 { getColor = Color 0.5 1 0.1
                                 , getDiffuse = 0.7
@@ -64,7 +66,7 @@ renderWorld = do
         light = PointLight (Point 3 5 (-6)) (Color 1 1 1)
         objects = [floor', leftSphere', middleSphere', rightSphere']
         world = World objects (Just light)
-        cameraTransform = viewTransform (Point 0 5 (-4)) (Point 0 1 0) (Vector 0 1 0)
+        cameraTransform = viewTransform (Point (-2) 5 (-4)) (Point 0 1 0) (Vector 0 1 0)
         camera = (createCamera 640 480 (pi / 3)) { getCameraTransform = cameraTransform }
         image = render camera world
     
